0ebe081423b24d45d60e1ee6dfe8cf4b1b62e5ad
codexcoder <parasmottan9316@gmail.com>
2025-06-13 18:06:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from 'react';
 // Socket.IO client import (install pehle karo: npm i socket.io-client)
 import { io } from 'socket.io-client';
 
-// Socket server ka URL (backend ka port 5000)
 const socket = io('https://ca-backend-p2rp.onrender.com');
 
 
@@ -11,13 +10,24 @@ const App = () => {
   // States to store message input aur messages list
   const [message, setMessage] = useState('');
   const [chat, setChat] = useState([]);
+  const [username, setUsername] = useState(''); // Static username, tu chahe to input se le sakta hai
+
+
 
   // useEffect tab chalega jab component mount hoga
   useEffect(() => {
+     const oldChat = localStorage.getItem('chat');
+  if (oldChat) {
+    setChat(JSON.parse(oldChat));
+  }
     // Jab server se 'receive_message' event aaye
     socket.on('receive_message', (data) => {
       // message list me naye message ko add karo
-      setChat((prevChat) => [...prevChat, data]);
+      setChat((prevChat) => {
+        const updatedChat = [...prevChat, data];
+      localStorage.setItem('chat', JSON.stringify(updatedChat)); // Store in LS
+      return updatedChat;
+      });
     });
 
     // Cleanup on unmount: listener hata do
@@ -27,12 +37,23 @@ const App = () => {
   // Form submit pe ye chalega
   const handleSend = (e) => {
     e.preventDefault();
+if (message.trim() === '') {
+  alert('Message cannot be empty!');
 
+  return;
+}
+else if (username.trim() === '') {
+  alert('Username cannot be empty!');
+  return;
+    }
+    
     // Ek object bana ke bhejna: username + message
     const data = {
-      username: 'Paras', // static user name, tu chahe to input se le sakta hai
+  
+        username: username, // static user name, tu chahe to input se le sakta hai
       message: message
-    };
+    }
+        
 
     // Server ko 'send_message' event emit karo
     socket.emit('send_message', data);
@@ -44,12 +65,23 @@ const App = () => {
     setMessage('');
   };
 
+
   return (
     <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
-      <h2>💬 Real-Time Chat (Socket.IO)</h2>
+      <h2>💬 Real-Time Chat </h2>
 
       {/* Message list */}
       <div style={{ border: '1px solid #ddd', padding: '10px', height: '300px', overflowY: 'auto' }}>
+           <form onSubmit={handleSend} style={{ marginTop: '10px' }}>
+        <input
+          type="text"
+          placeholder="Type your username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          style={{ padding: '5px', width: '20%' }}
+        />
+        
+      </form>
         {chat.map((msg, index) => (
           <div key={index} style={{ marginBottom: '10px' }}>
             <strong>{msg.username}:</strong> {msg.message}
